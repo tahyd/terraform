159dc931224085e9b8ef92295a2a634ef1d0ab45
tahyd <140394321+tahyd@users.noreply.github.com>
2025-04-14 12:25:21 +0530

    Add files via upload

---
+++ s3bucket.tf
@@ -0,0 +1,48 @@
+resource "aws_s3_bucket" "sample_bucket" {
+    bucket = "sample-demo-bucket"
+     tags = {
+        Name = "sample_bucket1108679044"
+        Description = "Sample bucket to store terraform state file"
+     }
+}
+
+resource "aws_s3_object" "sample_object" {
+
+    content = "<h1> Hello World ! </h1>"
+    key = "table.tf"
+    bucket = aws_s3_bucket.sample_bucket.id
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+       bucket = aws_s3_bucket.sample_bucket.id
+      block_public_acls = false
+      block_public_policy = false
+      ignore_public_acls = false
+      restrict_public_buckets = false
+    }
+
+resource "aws_s3_bucket_policy" "s3-policy" {
+   bucket = aws_s3_bucket.sample_bucket.id
+   policy = <<EOF
+       
+  
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.sample_bucket.id}/*",
+            "Principal" : {
+            "AWS": ["*"]
+             }
+        }
+    ]
+}
+
+
+
+
+    EOF
+}
+
