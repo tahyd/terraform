e5ce99bcf0e4080a4a57d9487b5d1a46759ae8e8
tahyd <140394321+tahyd@users.noreply.github.com>
2025-04-14 11:16:08 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,50 @@
+resource "aws_instance" "webserver" {
+    ami = "ami-084568db4383264d4"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Webserver"
+        Description = "an nginx machine"
+    }
+
+    user_data = <<-EOF
+         #!/bin/bash
+         sudo apt update
+         sudo apt install nginx -y
+         systemctl enable nginx
+         systemctl start nginx
+    EOF 
+
+    key_name = aws_key_pair.web_key.id
+vpc_security_group_ids = [aws_security_group.ssh-access.id]
+}
+
+
+resource "aws_key_pair" "web_key" {
+
+    public_key = file("C:/Users/HSBC/.ssh/id_ed25519.pub")
+}
+
+resource "aws_security_group" "ssh-access" {
+    name = "ssh-access"
+    description = "allow ssh connection from internet"
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.ssh-access.id
+ cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.ssh-access.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
